93c0a50590ee77847fe4cf87117656f58c655aff
Jeremy Morales <jeremy.morales@gmail.com>
2019-07-30 18:56:04 -0700

    fetch actions added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,35 @@
 import axios from 'axios';
 
-export const FETCH_USERS_START = "FETCH_USERS_START";
+export const FETCH_USERS_START = 'FETCH_USERS_START';
 export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_FAIL = 'FETCH_USERS_FAIL';
 
-export cosnt 
+export const FETCH_POSTS_START = 'FETCH_POSTS_START';
+export const FETCH_POSTS_SUCCESS = 'FETCH_POSTS_SUCCESS';
+export const FETCH_POSTS_FAIL = 'FETCH_POSTS_FAIL';
+
+export const fetchUsers = () => dispatch => {
+  dispatch({ type: FETCH_USERS_START });
+  axios
+    .get('http://localhost:4000/api/users')
+    .then(res => {
+      dispatch({ type: FETCH_USERS_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_USERS_FAIL, payload: err.message });
+    });
+};
+
+export const fetchUserPosts = userId => dispatch => {
+  dispatch({ type: FETCH_POSTS_START });
+  axios
+    .get(`http://localhost:4000/api/users/${userId}/posts`)
+    .then(res => {
+      dispatch({ type: FETCH_POSTS_START, payload: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: FETCH_POSTS_FAIL, payload: err.message });
+    });
+};
