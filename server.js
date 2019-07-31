5ad07a8ecf9a0352259f317309da5466a54bab5d
Jeremy Morales <jeremy.morales@gmail.com>
2019-07-30 18:44:57 -0700

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const userRouter = require('./users/UserRouter');
 const postRouter = require('./posts/postRouter');
@@ -6,6 +7,7 @@ const postRouter = require('./posts/postRouter');
 const server = express();
 server.use(logger);
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/users', userRouter);
 server.use('/api/posts', postRouter);
